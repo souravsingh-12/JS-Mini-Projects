178f8c9d9d2643561823aa4944889806281d5369
Sourav singh <singhsourav09024@gmail.com>
2025-09-19 00:11:35 +0530

    changes

---
+++ 03_AgeCalculator/script.js
@@ -0,0 +1,12 @@
+console.log("Connected");
+const dob = document.getElementById('date')
+const btn = document.querySelector("button")
+const ageDisplay = document.getElementById("resultDisplay")
+
+dob.max = new Date().toISOString().split("T")[0]
+
+function calculateAge(){
+   
+}
+
+
